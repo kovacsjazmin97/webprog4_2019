98b897b4963691e990f25c92e49d1846ed3175b6
Jazmin Kovacs <kovacsjazmin.97@gmail.com>
2019-09-24 09:30:52 +0200

    arrayReduceExample.js is modified
    arrayReduceExample.js is modified

---
+++ webstormDemoProject/arrayReduceExample.js
@@ -0,0 +1,16 @@
+var students = [
+{name : 'Alice', score : 23}
+{name : 'Bob', score : 57}
+{name : 'Charlie', score : 82}
+];
+
+console.log(
+student.redunce((total,current)=>{
+return total + current.score
+},0)
+);
+
+
+
+
+
